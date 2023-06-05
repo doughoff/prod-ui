2c12c09df9c16db6e3df7585444ddad4f3a53905
hoffax <douglashoffmann@outlok.es>
2023-06-05 18:33:06 -0400

    chore: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '../../../../../../vite.svg'
 import './App.css'
 const App: React.FunctionComponent = () => {
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
