0ff82038d1b08e45f8f83ce5df6885aba76035a6
hoffax <douglashoffmann@outlok.es>
2023-06-09 12:57:29 -0400

    login example

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,29 @@
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Card, Checkbox, Form, Input, Typography } from "antd";
 import React from "react";
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { login } from '../../api';
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+  remember: z.boolean().optional()
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
 
 const LoginPage: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <Card style={{ width: 420, padding: 24 }}>
@@ -13,35 +35,67 @@ const LoginPage: React.FC = () => {
           initialValues={{ remember: true }}
           autoComplete="off"
           layout="vertical"
+          onFinish={handleSubmit(data => {
+            // aqui chama a função login
+            // salva no localStorage email caso a pessoa marcou o remember
+            if (data.remember) {
+              // salva email no localstorage pra carregar automatico proxima vez
+            }
+
+            // chama login
+            login(data.email, data.password).then(res => {
+              //aqui foi success mandar pra home
+              console.log(res)
+            }).catch((err) => {
+              // aqui deu erro
+              // pesquisar como fazer o cast to AxiosError pra checar o err.response.data.message
+              // mostrat Toast > Email/Contrasena invalida
+              console.error(err)
+            })
+          })}
         >
           <Form.Item
-            name="username"
-            label="Correo Electronico"
-            style={{ marginBottom: "12px" }}
+            label="Correo Eletrónico"
+            validateStatus={errors.email ? 'error' : ''}
+            help={errors.email?.message}
           >
-            <Input />
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <Input prefix={<MailOutlined />} {...field} />}
+            ></Controller>
           </Form.Item>
 
           <Form.Item
-            name="password"
-            label="Contraseña"
-            style={{ marginBottom: "12px" }}
+            validateStatus={errors.password ? 'error' : ''}
+            help={errors.password?.message?.toString()}
           >
-            <Input.Password />
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <Input.Password prefix={<LockOutlined />} {...field} />}
+            ></Controller>
           </Form.Item>
 
           <Form.Item name="remember" valuePropName="checked">
-            <Checkbox>Recuerdame</Checkbox>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <Checkbox  {...field} />}
+            ></Controller>
           </Form.Item>
 
-          <Form.Item style={{ marginBottom: "0px" }}>
+          <Form.Item>
             <Button type="primary" htmlType="submit">
               Entrar
             </Button>
           </Form.Item>
         </Form>
       </Card>
-    </div>
+    </div >
   );
 };
 
