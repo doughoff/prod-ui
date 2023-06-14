6aa8cbf0958e37c4a94984c07725cf0cec2873ab
hoffax <douglashoffmann@outlook.es>
2023-06-13 22:01:20 -0400

    api-session

--- src/api/authRepository.ts
+++ src/api/authRepository.ts
@@ -1,24 +1,21 @@
-import { api, initializeApi } from "./apiClient";
+import { api } from './apiClient';
 
 const login = (email: string, password: string): Promise<boolean> => {
   return new Promise((resolve, reject) => {
     api
       .post<{
         session_id: string;
-      }>("/auth/login", {
+      }>('/auth/login', {
         email,
         password,
       })
       .then((res) => {
         if (res.data.session_id) {
-          localStorage.setItem("sessionID", res.data.session_id);
-          setTimeout(() => {
-            initializeApi();
-          }, 100);
+          localStorage.setItem('sessionID', res.data.session_id);
           resolve(true);
         } else {
-          console.error("api didnt return session id");
-          reject("err_login");
+          console.error('api didnt return session id');
+          reject('err_login');
         }
       })
       .catch((err) => {
@@ -28,16 +25,16 @@ const login = (email: string, password: string): Promise<boolean> => {
 };
 const logout = (): Promise<boolean> => {
   return new Promise((resolve, reject) => {
-    const sessionID = localStorage.getItem("sessionID");
+    const sessionID = localStorage.getItem('sessionID');
     if (!sessionID) {
       resolve(true);
       return;
     }
 
     api
-      .post("/auth/logout")
+      .post('/auth/logout')
       .then(() => {
-        localStorage.removeItem("sessionID");
+        localStorage.removeItem('sessionID');
         resolve(true);
       })
       .catch((err) => {
