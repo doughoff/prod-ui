82b85a65a0b48ed093ffaf7580272ae31c49aba1
hoffax <douglashoffmann@outlook.es>
2023-06-17 11:45:21 -0400

    fix: api client error handling

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import { message } from 'antd';
+import axios, { AxiosError } from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://127.0.0.1:3088',
@@ -18,10 +19,36 @@ api.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    if (error.response?.status === 401) {
+  (error: AxiosError) => {
+    if (!error) {
+      message.error(
+        'Ocurrió un error en el cliente. Por favor, póngase en contacto con el administrador.'
+      );
+      window.history.pushState({}, '', '/login');
+    }
+
+    // Network Connection error
+    else if (error.message && error.message === 'Network Error') {
+      message.error(
+        'Ocurrió un problema al conectarse a Internet. Por favor, compruebe su conexión.'
+      );
+    }
+    // RESPONSE ERROR WITHOUT SPECIFICATIONS
+    else if (!error.response) {
+      message.error(
+        'No fue posible interpretar el error. Por favor, póngase en contacto con el administrador.'
+      );
+    }
+
+    if (error?.response?.status && error.response?.status === 401) {
       localStorage.removeItem('sessionID');
       window.location.reload();
+    }
+
+    // FORBIDEN AUTHENTICATION
+    else if (error.response?.status && error.response.status === 403) {
+      message.error('Access to the screen blocked!');
+      window.history.back();
     }
 
     return Promise.reject(error);
